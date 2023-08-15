72961fe25720736e47d9b8965dbf56452d9cb6e5
Gustavo Schonarth <gustavo.schonarth@meta.com.br>
2023-08-15 19:42:36 -0300

    Add Whitespectre's modal
    * Simplest implementation, just following example
    * TS from example converted to JS for simplicity

--- App.js
+++ App.js
@@ -4,6 +4,8 @@ import React, { useState } from 'react'
 import { Button, StyleSheet, Text, View } from 'react-native'
 import FieldWrapper from './FieldWrapper.component'
 import Modal from './Modal.component'
+import { ModalPresenterParent } from '@whitespectre/rn-modal-presenter'
+import { showCustomAlert } from './CustomAlert.component'
 
 const button1Text = 'React Native standard modal'
 const button2Text = 'Whitespectre modal'
@@ -13,58 +15,78 @@ const App = () => {
     setModalVisible(true)
   }
 
-  const handleOpenWhitespectreModal = () => {}
+  const handleOpenWhitespectreModal = () => {
+    showAlertFromAlert()
+  }
 
   const [modalVisible, setModalVisible] = useState(false)
 
   const [currentText, setCurrentText] = useState('')
   const [ontheflyUpdate, setOntheflyUpdate] = useState(undefined)
 
+  const showAlertFromAlert = async () => {
+    const modalHandler = await showCustomAlert({
+      title: 'Alert from alert',
+      body: 'This is an alert from an alert',
+      buttons: [
+        {
+          title: 'OK',
+          action: () => {
+            console.log('OK Pressed')
+            modalHandler.dismiss()
+          },
+        },
+      ],
+    })
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>
-        The same field wrapper (also below) is in a regular Modal and in a
-        Whitespectre modal
-      </Text>
-      <View style={{ margin: 20 }}>
-        <Button title={button1Text} onPress={handleOpenStandardModal} />
-        <Button title={button2Text} onPress={handleOpenWhitespectreModal} />
-      </View>
-      <StatusBar style="auto" />
-      <View>
+    <ModalPresenterParent>
+      <View style={styles.container}>
         <Text>
-          App state: <Text style={{ fontWeight: 'bold' }}>{currentText}</Text>
+          The same field wrapper (also below) is in a regular Modal and in a
+          Whitespectre modal
         </Text>
-        <Text>
-          Field on the fly update:{' '}
-          <Text style={{ fontWeight: 'bold' }}>{ontheflyUpdate}</Text>
-        </Text>
-        <FieldWrapper
-          value={currentText}
-          onBlur={value => {
-            setCurrentText(value)
-            setOntheflyUpdate(undefined)
+        <View style={{ margin: 20 }}>
+          <Button title={button1Text} onPress={handleOpenStandardModal} />
+          <Button title={button2Text} onPress={handleOpenWhitespectreModal} />
+        </View>
+        <StatusBar style="auto" />
+        <View>
+          <Text>
+            App state: <Text style={{ fontWeight: 'bold' }}>{currentText}</Text>
+          </Text>
+          <Text>
+            Field on the fly update:{' '}
+            <Text style={{ fontWeight: 'bold' }}>{ontheflyUpdate}</Text>
+          </Text>
+          <FieldWrapper
+            value={currentText}
+            onBlur={value => {
+              setCurrentText(value)
+              setOntheflyUpdate(undefined)
+            }}
+            onChange={setOntheflyUpdate}
+          />
+        </View>
+        <Modal
+          visible={modalVisible}
+          onDismiss={() => {
+            setModalVisible(false)
+            setOntheflyUpdate(undefined) // just in case
           }}
-          onChange={setOntheflyUpdate}
-        />
+        >
+          <FieldWrapper
+            value={currentText}
+            onBlur={value => {
+              setCurrentText(value)
+              setOntheflyUpdate(undefined)
+            }}
+            onChange={setOntheflyUpdate}
+          />
+        </Modal>
       </View>
-      <Modal
-        visible={modalVisible}
-        onDismiss={() => {
-          setModalVisible(false)
-          setOntheflyUpdate(undefined) // just in case
-        }}
-      >
-        <FieldWrapper
-          value={currentText}
-          onBlur={value => {
-            setCurrentText(value)
-            setOntheflyUpdate(undefined)
-          }}
-          onChange={setOntheflyUpdate}
-        />
-      </Modal>
-    </View>
+    </ModalPresenterParent>
   )
 }
 
---
+++ CustomAlert.component.jsx
@@ -0,0 +1,97 @@
+import React from 'react'
+import { View, StyleSheet, Text, Button } from 'react-native'
+import {
+  ModalContentProps,
+  showModal,
+  enqueueModal,
+} from '@whitespectre/rn-modal-presenter'
+
+const defaultButton = { title: 'Got It' }
+
+export const showCustomAlert = ({
+  title,
+  body,
+  buttons = [defaultButton],
+  enqueue = false,
+}) => {
+  console.log('showCustomAlert', title, body, buttons, enqueue)
+  if (enqueue) {
+    return enqueueModal(CustomAlert, {
+      title,
+      body,
+      buttons,
+    })
+  } else {
+    return showModal(CustomAlert, {
+      title,
+      body,
+      buttons,
+    })
+  }
+}
+
+const CustomAlert = ({ dismiss, title, body, buttons }) => {
+  console.log('CustomAlert', title, body, buttons)
+  return (
+    <View style={styles.container}>
+      <View style={styles.modal}>
+        <View style={styles.contentContainer}>
+          <Text style={styles.title}>{title}</Text>
+          <Text style={styles.detail}>{body}</Text>
+          {buttons.map((button, index) => {
+            return (
+              <View key={index} style={styles.button}>
+                <Button
+                  title={button.title}
+                  onPress={button.action ?? (() => dismiss())}
+                />
+              </View>
+            )
+          })}
+        </View>
+      </View>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  container: {
+    width: '100%',
+    paddingHorizontal: 16,
+  },
+  modal: {
+    borderRadius: 16,
+    backgroundColor: 'white',
+    width: '100%',
+    overflow: 'hidden',
+  },
+  contentContainer: {
+    paddingHorizontal: 24,
+    paddingTop: 33,
+    paddingBottom: 40,
+    alignItems: 'center',
+  },
+  title: {
+    textAlign: 'center',
+    marginHorizontal: 30,
+    fontSize: 19,
+    lineHeight: 28,
+  },
+  detail: {
+    marginTop: 16,
+    textAlign: 'center',
+    fontSize: 16,
+    lineHeight: 26,
+  },
+  button: {
+    width: '100%',
+    marginTop: 32,
+  },
+  closeButtonContainer: {
+    position: 'absolute',
+    top: 14,
+    right: 30,
+  },
+})
+
+export default CustomAlert
